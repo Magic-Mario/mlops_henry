0cd67385c7faa42511f3edf2e777c5856d48df89
Candelario-Hudson <magic.mario2021@gmail.com>
2023-06-10 17:42:00 -0500

    Trying again with gunicorn

--- start.sh
+++ start.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-uvicorn main_api:app --host 0.0.0.0 --port $PORT
+
+# Activa el entorno virtual si lo estás utilizando
+# source path/to/virtualenv/bin/activate
+
+# Establece el valor de $PORT si no está configurado
+if [[ -z "$PORT" ]]; then
+  export PORT=8000
+fi
+
+# Inicia la aplicación con Gunicorn
+gunicorn -w 4 -k uvicorn.workers.UvicornWorker your_app_module:app -b 0.0.0.0:$PORT
+
